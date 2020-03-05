4bd27e91f01389692af6c0437eb739a8e58f4eea
Said Bakr <said.fox@gmail.com>
2020-03-05 15:33:20 +0200

    Introducing optional log param to display log only

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ Simple Python3 script to update [Freemyip.com](https://freemyip.com) dynamic DNS
        domain2.freemyip.com,TOKEN_OF_DOMAIN2
        ```
  3. Run the script `python3 freemyip.py`
+ 4. The script could able to show the last log data without update using `log` as a parameter. i.e:
+ 
+      ```
+      python3 freemyip.py log
+      ```
  
  **Hint**: The above command can be added to crontab in Linux-based operating systems or any equivalent tool in other operating systems to keep automatic host update.
 
--- freemyip.py
+++ freemyip.py
@@ -7,6 +7,7 @@ from requests import get
 import csv
 import re
 import time
+import sys
 # Configurations values
 base_url = 'https://freemyip.com/update'
 tokens_file = 'tokens.csv'
@@ -114,13 +115,47 @@ def create_log(log_file):
     f.close()
     print('Log file ['+log_file+'] has been created.')
 
+def print_log(log_file):
+    output = 'The log file below:\n'
+    try:
+        f = open(log_file,'r')
+    except IOError:
+        print('The log file [{}] could not be opened.'.format(log_file))
+        exit()
+    
+    output += f.read()
+    f.close()
+    output += '===== \nShow log is finished.'
+    return output
+
+
 #Executing the script
+param = None
+try:
+    param = sys.argv[1]
+    if param == 'log':
+        print(print_log(log_file))
+        exit()
+    else:
+        print('Not supported parameter value.')
+        exit()
+    
+    
+except:
+    if param != None:
+        exit()
+
+
+
+
 load_tokens(tokens_file)
 public_ip = public_ip()
-print('Domain'+'\t\t\t'+'Status'+'\t'+'IP')
+print('Domain'+'\t\t\t'+'Status'+'\t\t'+'IP')
 for domain,token in data.items():
     print(domain,'\t',check_url(create_url(domain, token),domain, public_ip),'\t', ip)   
 print('=====\nUpdate has been done!')
 print('*N.R.U: Not Require Update. i.e. the public IP still the same in the last [{}] seconds.'.format(no_update_time))
 
 create_log(log_file)
+
+
